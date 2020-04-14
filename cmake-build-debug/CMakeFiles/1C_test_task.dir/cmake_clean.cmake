4de684bf9fe90d48e48fc772047421e3d110f3d5
Nikita <kononenko.n@phystech.edu>
2020-04-14 14:45:07 +0300

    prepare for coding

---
+++ cmake-build-debug/CMakeFiles/1C_test_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1C_test_task.dir/main.cpp.o"
+  "1C_test_task.pdb"
+  "1C_test_task"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1C_test_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/1C_test_task.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1C_test_task.
+# This may be replaced when dependencies are built.
